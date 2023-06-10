fba6aac591abaf10d3657f18821cfc4fb64ae2c5
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-06-11 01:41:09 +0600

    level01-additional complete

--- src/components/Level01/Level01.tsx
+++ src/components/Level01/Level01.tsx
@@ -1,19 +1,28 @@
+import { nanoid } from "nanoid";
 import React, { useState } from 'react';
+import { IButtonsConfig } from "../../types";
 import Modal from "./Modal/Modal";
 
-const Level01 = () => {
+interface Props {
+	isModal: boolean;
+	cancel: () => void;
+	continueToAlert: () => void;
+}
 
-	const [isShow, setIsShow] = useState<boolean>(false);
-
-	const cancel = (): void => setIsShow(!isShow);
+const Level01: React.FC<Props> = ({ isModal, cancel, continueToAlert }) => {
+	const buttonsConfig: IButtonsConfig[] = [
+		{type: 'primary', label: 'Go to second level', onClick: continueToAlert, id: nanoid()},
+		{type: 'danger', label: 'Close', onClick: cancel, id: nanoid()}
+	];
 
 	return (
 		<div className="text-center">
 			<button className="btn btn-outline-primary" onClick={cancel}>Show</button>
 			<Modal
-				show={isShow}
+				show={isModal}
 				onClose={cancel}
 				title="Some kinda modal title"
+				configs={buttonsConfig}
 			>
 				<p>This is modal content</p>
 			</Modal>
--- src/components/Level01/Modal/Modal.tsx
+++ src/components/Level01/Modal/Modal.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
+import { IButtonsConfig } from "../../../types";
 
 interface Props extends React.PropsWithChildren {
 	show: boolean;
 	onClose: React.MouseEventHandler;
 	title: string;
+	configs: IButtonsConfig[];
 }
 
-const Modal: React.FC<Props> = ({show, title, children, onClose }) => {
+const Modal: React.FC<Props> = ({show, title, children, onClose, configs }) => {
 	return (
 		<div className="modal show" style={{display: show ? 'block' : 'none'}}>
 			<div className="modal-dialog">
 				<div className="modal-content">
+
 					<div className="modal-header">
 						<h1 className="modal-title fs-5">{ title }</h1>
 						<button className="btn btn-close" onClick={onClose}></button>
 					</div>
+
 					{ children }
+
+					<div className="modal-footer">
+						{
+							configs.map((config: IButtonsConfig) => {
+								return <button className={`btn btn-${config.type}`} onClick={config.onClick} key={config.id}>{config.label}</button>
+							})
+						}
+					</div>
+
 				</div>
 			</div>
 		</div>
